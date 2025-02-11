e5d351fb002172003fd4ba48785bfb12a90dddac
Roshani Ahir <roshaniahir3@gmail.com>
2025-02-11 17:22:15 +0530

    edited src/main

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         Date currentDate = new Date();
-        System.out.println("Hello,my name is Akash Current date: " + currentDate);
+        System.out.println("Hello, My name is Akash and we are practicing Docker! Current date: " + currentDate);
     }
 }
